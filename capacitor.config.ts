e3fc6488a73894a5074d109c71fc95fc8d66520b
Dines Nimalthas <dines_nimalthas@sluz.ch>
2024-01-23 14:28:06 +0100

    initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'm335-project',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
