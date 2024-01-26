cf95849a558755790b7498544a0352d8fa5ba8e0
Dines Nimalthas <dines_nimalthas@sluz.ch>
2024-01-26 12:10:43 +0100

    Finished

--- src/app/task1/task1.page.ts
+++ src/app/task1/task1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Geolocation } from '@capacitor/geolocation';
